963bcfef28d81bf467273e2f59a57fc2684bda4e
namatlan <namdo2407@gmail.com>
2021-04-12 13:42:09 +0700

    update demo pyhton + git

--- TestCases/TIKI_Search_TC_001.py
+++ TestCases/TIKI_Search_TC_001.py
@@ -5,54 +5,73 @@ import time
 
 
 class TestHomePage(unittest.TestCase):
-
     @classmethod
     def setUp(self):
-    #     # Initiate in "headless mode", where the browser saves time by not rendering the page:
-    #     # from selenium.webdriver.chrome.options import Options
-    #     #
-    #     # options = Options ()
-    #     # options.headless = True
-    #     # driver = webdriver.Chrome ( executable_path=EXE_PATH, chrome_options=options )
-    #     # driver.set_window_size ( 1440, 900 )
-    #     self.driver.get ( BASE_URL )
-        self.driver = webdriver.Chrome ( executable_path="..//drivers//chromedriver.exe" )
-        self.driver.maximize_window ()
-        self.driver.implicitly_wait ( 15 )
+        #     # Initiate in "headless mode", where the browser saves time by not rendering the page:
+        #     # from selenium.webdriver.chrome.options import Options
+        #     #
+        #     # options = Options ()
+        #     # options.headless = True
+        #     # driver = webdriver.Chrome ( executable_path=EXE_PATH, chrome_options=options )
+        #     # driver.set_window_size ( 1440, 900 )
+        #     self.driver.get ( BASE_URL )
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--disable-infobars")
+        prefs = {"profile.default_content_setting_values.notifications": 2}
+        chrome_options.add_experimental_option("prefs", prefs)
+        self.driver = webdriver.Chrome(
+            executable_path="drivers//chromedriver.exe", options=chrome_options)
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(5)
+        self.driver.set_page_load_timeout(30)
         self.driver.get("https://tiki.vn/")
-        #print(self.driver.title)
 
-    @classmethod
     def test_search_with_keyword(self):
-        elem = self.driver.find_element_by_css_selector (
-            '#__next > div > header > div.Container-sc-1fnsswg-0.itgICN > div > div.Middle__LeftContainer-sc-1poi789-2.iXVTaA > div.FormSearch__Root-hwmlek-0.eqgiix > div > input' )
-        elem.click ()
-        elem.send_keys ( 'Dell laptop' )
-        elem = self.driver.find_element_by_css_selector (
-            '#__next > div > header > div.Container-sc-1fnsswg-0.itgICN > div > div.Middle__LeftContainer-sc-1poi789-2.iXVTaA > div.FormSearch__Root-hwmlek-0.eqgiix > div > button' )
-        elem.click ()
-        elem = self.driver.find_element_by_css_selector (
-            '#ants-product-list > div:nth-child(1) > a > div.content > span > img' )
-        elem.click ()
-
-        elem = self.driver.find_element_by_css_selector ( '#\#mainAddToCart' )
-        time.sleep ( 2 )
-        elem.click ()
-        elem = self.driver.find_element_by_css_selector ( '#header-cart > a > div > button' )
-        elem.click ()
-        elem = self.driver.find_element_by_css_selector (
-            '#shopping-cart > div > div.col-right > div.box-info-product > p.action > a.btn.btn-link.btn-item-delete' )
-        elem.click ()
-        elem = self.driver.find_element_by_css_selector (
-            'body > div.wrap > div > div:nth-child(3) > div > div > a' )
-        elem.click ()
-        self.driver.save_screenshot ( 'C:/Selenium/Python/Project_Tiki/Screenshots/SearchDone.png' )
-        print ( "Search Product Successfully" )
+        elem = self.driver.find_element_by_xpath(
+            "//*[@id='__next']/div[1]/header/div[1]/div/div[1]/div[2]/div/input"
+        )
+        elem.click()
+        elem.send_keys("Dell laptop")
+        elem = self.driver.find_element_by_xpath(
+            "//*[@id='__next']/div[1]/header/div[1]/div/div[1]/div[2]/div/button/img"
+        )
+        elem.click()
+        time.sleep(3)
+        
+        # Chọn Hàng mới của Dell laptop
+        elem_hangmoi = self.driver.find_element_by_xpath(
+            "//*[@id='__next']/div[1]/main/div[2]/div/div[2]/div/div[1]/div[2]/div[1]/div/div/a[3]"
+        )
+        elem_hangmoi.click()
+        time.sleep(3)
+        lst_products = self.driver.find_elements_by_xpath(
+            "//*[@id='__next']/div[1]/main/div[2]/div/div[2]/div/div[2]//a[@class='product-item']"
+        )
+        get_products = [item for item in lst_products]
+        print(get_products)
+        get_products[1].click()
+        time.sleep(4)
+        chonmua_btn = self.driver.find_element_by_xpath(
+            "//*[@id='__next']/div[1]/main/div[4]/div/div[3]/div[2]/div/div[1]/div[4]/div[2]/button"
+        )
+        chonmua_btn.click()
+        time.sleep(2)
+        xem_gio_hang_btn = self.driver.find_element_by_xpath(
+            "//*[@id='__next']/div[1]/header/div[1]/div/div[2]/div[2]/div/a[2]"
+        )
+        xem_gio_hang_btn.click()
+        time.sleep(3)
+        check_price = self.driver.find_element_by_xpath("//*[@id='__next']/div[1]/main/div/div/div/div[1]/div/div/div/ul/li/div/div[2]/div/div[2]/div[1]/p")
+        if check_price.text >= '17.000.000':
+            print('Real price:', check_price.text.encode("utf-8"))
+        self.driver.save_screenshot("Screenshots/SearchDone.png")
+        print("Search Product Successfully")
 
     @classmethod
     def tearDown(self):
         self.driver.quit()
 
 
-if __name__ == '__main__':
-    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner ( output='C:/Selenium/Python/Project_Tiki/ReportsHTML' ))
+if __name__ == "__main__":
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(
+        output="ReportsHTML"))
--- TestCases/Test_Login_POM.py
+++ TestCases/Test_Login_POM.py
@@ -4,23 +4,24 @@ import unittest
 import time
 from Pages.LoginPage import Login
 import sys
-sys.path.append("C://Selenium//Python//Project_Tiki")
+sys.path.append("../Pages")
 
 
 class TestDemo(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.driver = webdriver.Chrome ( executable_path="..//drivers//chromedriver.exe" )
-        self.driver.maximize_window ()
+        self.driver = webdriver.Chrome(
+            executable_path="..//drivers//chromedriver.exe")
+        self.driver.maximize_window()
 
     def tearDown(self) -> None:
         self.driver.quit()
 
     def test_login_as_normal_user(self):
         driver = self.driver
-        driver.get ( "https://tiki.vn/" )
+        driver.get("https://tiki.vn/")
         assert "Tiki" in driver.title
-        driver.implicitly_wait ( 15 )
+        driver.implicitly_wait(15)
         log_in = Login(self.driver)
         log_in.click_1()
         log_in.click_2()
@@ -31,4 +32,5 @@ class TestDemo(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner ( output='C:/Selenium/Python/Project_Tiki/ReportsHTML'))
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(
+        output='ReportsHTML'))
